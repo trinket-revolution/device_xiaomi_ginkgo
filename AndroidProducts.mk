7ea47a43551bdd2439276fbbe3e6f88ec6c3bb5f
TheHitMan7 <krtik.vrma@gmail.com>
2020-04-25 16:13:23 +0530

    ginkgo: Syberia Bringup
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_ginkgo.mk
+    $(LOCAL_DIR)/syberia_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    syberia_ginkgo-user \
+    syberia_ginkgo-userdebug \
+    syberia_ginkgo-eng
---
+++ syberia_ginkgo.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/ginkgo/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/syberia/common.mk)
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 2160
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := syberia_ginkgo
+PRODUCT_DEVICE := ginkgo
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 8
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="ginkgo-user 9 PKQ1.190616.001 V10.3.3.0.PCOINXM release-keys" \
+    PRODUCT_NAME="ginkgo" \
+    TARGET_DEVICE="ginkgo"
+
+BUILD_FINGERPRINT := "xiaomi/ginkgo/ginkgo:9/PKQ1.190616.001/V10.3.3.0.PCOINXM:user/release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
