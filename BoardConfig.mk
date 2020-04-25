279b30379e77cce5ef7585224f9980e364e6e27f
Ashwin R C <ashwin2001achu@gmail.com>
2020-04-25 13:51:33 +0530

    ginkgo: Disable vbmeta verification

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -223,6 +223,7 @@ TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 
 # WiFi
