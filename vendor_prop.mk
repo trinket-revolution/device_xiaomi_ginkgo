a4aa19e2f4c3982d07335f45509ae7b825196891
TheHitMan7 <krtik.vrma@gmail.com>
2020-05-08 09:50:20 +0530

    ginkgo: Use vulkan as default renderer
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -154,3 +154,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_app_phase_offset_ns=500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.use_vulkan=true \
+    debug.hwui.renderer=skiavk
