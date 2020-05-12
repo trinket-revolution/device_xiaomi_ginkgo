d5431d06fb19fb9dd8c4c8835d86ea2b053ed496
Rama Bondan Prakoso <ramanarubp@gmail.com>
2020-05-12 04:00:37 +0000

    Revert "ginkgo: Use vulkan as default renderer"
    This reverts commit a4aa19e2f4c3982d07335f45509ae7b825196891.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -154,7 +154,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_app_phase_offset_ns=500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.use_vulkan=true \
-    debug.hwui.renderer=skiavk
